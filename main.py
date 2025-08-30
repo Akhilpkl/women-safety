338f28693d2818edbcd261c0c71224e7672b7060
Akhilpkl <akhilshajipkl@gmail.com>
2025-08-30 13:39:57 +0530

    homegae ,main

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-hello
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("loginpage.html")  
+
+if __name__ == "__main__":
+    app.run(debug=True)
